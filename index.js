e27fbe30520dd4605023b89e0e9962b91344d1c6
Mathieu <mrobert@w-inn.eu>
2018-02-23 17:42:50 +0100

    heedf

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 
-// Server index page
-app.get("/", function (req, res) {
-  res.send("158133785");
-});
 
 // Facebook Webhook
 // Used for verification
