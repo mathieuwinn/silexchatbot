090482249c5f73701461ad148622711a3545416a
Mathieu <mrobert@w-inn.eu>
2018-02-23 17:46:07 +0100

    heedfsd

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 
+// Server index page
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
 
 // Facebook Webhook
 // Used for verification
-app.get("/", function (req, res) {
+app.get("/webhook", function (req, res) {
   if (req.query["hub.verify_token"] === "myCustomToken123") {
     console.log("Verified webhook");
     res.status(200).send(req.query["hub.challenge"]);
