eb62605efaab3b67441871b361cac02eabe5c415
Mathieu <mrobert@w-inn.eu>
2018-02-25 15:18:32 +0100

    heedfsddj

--- index.js
+++ index.js
@@ -7,32 +7,69 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 app.listen((process.env.PORT || 5000));
 
-// Server index page
-app.get("/", function (req, res) {
-  res.send("Deployed!");
-});
 
-// Facebook Webhook
-// Used for verification
-app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === "myCustomToken123") {
-    console.log("Verified webhook");
-    res.status(200).send(req.query["hub.challenge"]);
-  } else {
-    console.error("Verification failed. The tokens do not match.");
-    res.sendStatus(403);
+
+// All callbacks for Messenger will be POST-ed here
+app.post("/webhook", function (req, res) {
+  // Make sure this is a page subscription
+  if (req.body.object == "page") {
+    // Iterate over each entry
+    // There may be multiple entries if batched
+    req.body.entry.forEach(function(entry) {
+      // Iterate over each messaging event
+      entry.messaging.forEach(function(event) {
+        if (event.postback) {
+          processPostback(event);
+        }
+      });
+    });
+
+    res.sendStatus(200);
   }
 });
 
+function processPostback(event) {
+  var senderId = event.sender.id;
+  var payload = event.postback.payload;
 
+  if (payload === "Greeting") {
+    // Get user's first name from the User Profile API
+    // and include it in the greeting
+    request({
+      url: "https://graph.facebook.com/v2.6/" + senderId,
+      qs: {
+        access_token: process.env.PAGE_ACCESS_TOKEN,
+        fields: "first_name"
+      },
+      method: "GET"
+    }, function(error, response, body) {
+      var greeting = "";
+      if (error) {
+        console.log("Error getting user's name: " +  error);
+      } else {
+        var bodyObj = JSON.parse(body);
+        name = bodyObj.first_name;
+        greeting = "Hi " + name + ". ";
+      }
+      var message = greeting + "My name is SP Movie Bot. I can tell you various details regarding movies. What movie would you like to know about?";
+      sendMessage(senderId, {text: message});
+    });
+  }
+}
 
-
-curl -X POST -H "Content-Type: application/json" -d '{
-  "setting_type":"call_to_actions",
-  "thread_state":"new_thread",
-  "call_to_actions":[
-    {
-      "payload":"Greeting"
+// sends message to user
+function sendMessage(recipientId, message) {
+  request({
+    url: "https://graph.facebook.com/v2.6/me/messages",
+    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+    method: "POST",
+    json: {
+      recipient: {id: recipientId},
+      message: message,
     }
-  ]
-}' "https://graph.facebook.com/v2.6/me/thread_settings?access_token=EAAcSEWH7Fs4BALAuY0xZAHk1zX5YV5t1AS2PuY8lY7V0gnbau1USKYQBQP5eiF3mgp18TpbZCWqH3nwF0JBOhIZCnT4FhLUGiwmmriVvf1fGRXmhjw6lNXWIImW4TXQU2F6hsssDYQpWqpk6djcr6TjrJhStXoMZBfZAcz4aelAZDZD"
+  }, function(error, response, body) {
+    if (error) {
+      console.log("Error sending message: " + response.error);
+    }
+  });
+}
